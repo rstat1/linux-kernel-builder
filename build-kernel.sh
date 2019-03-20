e204f6392c2307f4d271f4f3129c9867294e61a2
rstat1 <git@rdro.us>
2019-03-20 13:42:19 -0400

    start

---
+++ build-kernel.sh
@@ -0,0 +1,12 @@
+VERSION_PREFIX="v5.x"
+LATEST_VERSION="5.0.3"
+
+mkdir kernel-build
+wget https://cdn.kernel.org/pub/linux/kernel/${VERSION_PREFIX}/linux-${LATEST_VERSION}.tar.xz
+mv linux-${LATEST_VERSION}.tar.xz kernel-build/
+cd kernel-build
+tar xJf linux-${LATEST_VERSION}.tar.xz
+
+cp ../.config linux-${LATEST_VERSION}/
+cd linux-${LATEST_VERSION}
+make -j4
